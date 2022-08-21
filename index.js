7cdf7b5aeb483a166cbbcf3f772c91afeedfd080
OtienohOwinoh <coowinoh@gmail.com>
2022-08-21 17:09:04 +0300

    COmplete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(function (record) {
+      return record.result === "W";
+    });
+    if (result) {
+      return result.year;
+    } else {
+      return undefined;
+    }
+  }
+  console.log(superbowlWin(record));
